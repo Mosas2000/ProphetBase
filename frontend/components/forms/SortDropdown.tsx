98396412f902be86923eeca26d3c8722c01407c5
0xMosas <tosinolosasa@gmail.com>
2026-01-20 01:54:29 +0100

    feat: add sort dropdown component

---
+++ frontend/components/forms/SortDropdown.tsx
@@ -0,0 +1,29 @@
+'use client';
+
+import Select from '@/components/ui/Select';
+import { BarsArrowDownIcon } from '@heroicons/react/24/outline';
+import { forwardRef } from 'react';
+
+interface SortDropdownProps extends React.ComponentProps<typeof Select> { }
+
+const SortDropdown = forwardRef<HTMLSelectElement, SortDropdownProps>(
+    ({ children, className = '', ...props }, ref) => {
+        return (
+            <div className="relative min-w-[150px]">
+                <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                    <BarsArrowDownIcon className="h-4 w-4 text-gray-500" />
+                </div>
+                <Select
+                    ref={ref}
+                    className={`pl-9 ${className}`}
+                    {...props}
+                >
+                    {children}
+                </Select>
+            </div>
+        );
+    }
+);
+
+SortDropdown.displayName = 'SortDropdown';
+export default SortDropdown;
